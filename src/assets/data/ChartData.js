0b060055190ee4db0aa34311925a400e2df2ef20
pmita <panos.mita2@outlook.com>
2021-08-17 16:24:56 +0100

    adding a Chart component with reChart library

---
+++ src/assets/data/ChartData.js
@@ -0,0 +1,26 @@
+export const userData = [
+    {
+        name: 'Jan',
+        "Active User": 4000,
+      },
+      {
+        name: 'Feb',
+        "Active User": 3000,
+      },
+      {
+        name: 'Mar',
+        "Active User": 5000,
+      },
+      {
+        name: 'Apr',
+        "Active User": 5500,
+      },
+      {
+        name: 'May',
+        "Active User": 5000,
+      },
+      {
+        name: 'Jun',
+        "Active User": 6000,
+      },
+]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 //importing our components
 import FeaturedInfo from '../components/FeaturedInfo';
+import Chart from '../components/Chart';
+//importing our assets
+import {userData} from '../assets/data/ChartData';
 
 export default function Home() {
     return (
         <section className="home">
             <FeaturedInfo />
+            <Chart 
+                   title="User Analytics"
+                   data = {userData}
+                   dataKey = "Active User"
+                   grid = {true} 
+            />
         </section>
     )
 }
